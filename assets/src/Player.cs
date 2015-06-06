164b13b96c0844358a086a8e13f657f3cda6d224
Richman Stewart <flynnspixel@gmail.com>
2015-06-06 13:31:08 +1000

    Basic wasd test movement

--- assets/src/Player.cs
+++ assets/src/Player.cs
@@ -3,12 +3,38 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+	Vector3 pos;
+	Vector2 accel;
+	float angle = 90;
+
+	const float radians = Mathf.PI / 180.0f;
+	const float max_speed = .25f;
+	const float friction = .98f;
+	const float turn_speed = 2;
+
 	void Start() {
-        Debug.Log("player start");
+		pos = transform.position;
 	}
 
 	void Update() {
-		transform.Translate(Input.GetKey(KeyCode.D) ? .01f : Input.GetKey(KeyCode.A) ? -.01f : 0, 0, 0);
-		transform.Translate(0, 0, Input.GetKey(KeyCode.W) ? .01f : Input.GetKey(KeyCode.S) ? -.01f : 0);
+		if (Input.GetKey(KeyCode.W)) {
+			accel.x -= Mathf.Cos(angle * radians) * .01f;
+			accel.y -= Mathf.Sin(angle * radians) * .01f;
+			accel.x = Mathf.Clamp(accel.x, -max_speed, max_speed);
+			accel.y = Mathf.Clamp(accel.y, -max_speed, max_speed);
+		}
+		if (Input.GetKey(KeyCode.A)) {
+			transform.Rotate(0, 0, turn_speed);
+			angle += turn_speed;
+		}else if (Input.GetKey(KeyCode.D)) {
+			transform.Rotate(0, 0, -turn_speed);
+			angle -= turn_speed;
+		}
+		accel *= friction;
+
+		pos.x += accel.x;
+		pos.z += accel.y;
+
+		transform.position = pos;
 	}
 }
