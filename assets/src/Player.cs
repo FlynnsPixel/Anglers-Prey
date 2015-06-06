4243608048f03d6a6ab0aabcaecbf83027e82f10
Richman Stewart <flynnspixel@gmail.com>
2015-06-06 12:45:39 +1000

    Added gitignore and multiple unity project files

---
+++ assets/src/Player.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	void Start() {
+        Debug.Log("player start");
+	}
+
+	void Update() {
+		transform.Translate(Input.GetKey(KeyCode.D) ? .01f : Input.GetKey(KeyCode.A) ? -.01f : 0, 0, 0);
+		transform.Translate(0, 0, Input.GetKey(KeyCode.W) ? .01f : Input.GetKey(KeyCode.S) ? -.01f : 0);
+	}
+}
